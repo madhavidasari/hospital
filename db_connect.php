e67b99cd09c6f742b0f90b989b0e7b8692efd5ff
madhavidasari <dasari.madhavi6@gmail.com>
2016-12-26 18:26:48 -0600

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -1,5 +1,5 @@
 <?php
-$conn = mysql_connect("localhost","root","") or die(mysql_error());
-mysql_select_db("hosp", $conn)or die(mysql_error());
+$conn = mysql_connect("hospserver.database.windows.net","madhan","Welcome2server!") or die(mysql_error());
+mysql_select_db("myphpdatabase", $conn)or die(mysql_error());
 date_default_timezone_set('America/Chicago');
 ?>
